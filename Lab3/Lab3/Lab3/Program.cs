648c000a8efb4b2e7b0eb2e990ab47bfdf91940b
Ewgen4ik100 <78424896+Ewgen4ik100@users.noreply.github.com>
2021-07-14 15:23:18 +0300

    Add files via upload
    Lab3

---
+++ Lab3/Lab3/Lab3/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Lab3
+{
+    class ClassTest
+    {
+        static void Main(string[] args)
+        {
+            ClassLibrary.Posled();
+        }
+    }
+    public class ClassLibrary
+    {
+        public static void Posled()
+        {
+            int rez = 0;
+            int ell;
+            int[] arr1 = new int[5];
+            int[] arr2 = new int[5];
+            for (int i = 0; i < 5; i++)
+            {
+                arr1[i] = int.Parse(Console.ReadLine());
+                arr2[i] = arr1[i];
+            }
+            Array.Sort(arr2);
+            for (int i = 0; i < 5; i++)
+            {
+                if(arr1[i] == arr2[i])
+                {
+                    ell = arr1[2];
+                    arr1[i] = arr1[i] * ell;
+                }
+                else
+                {
+                    Console.WriteLine(arr1[i]);
+                }
+            }
+            Console.WriteLine($"Индекс среднего элемента 2, значение {arr2[2]}\nРезультат умножения:{arr1[0]} {arr1[1]} {arr1[2]} {arr1[3]} {arr1[4]}");
+        }
+    }
+}
