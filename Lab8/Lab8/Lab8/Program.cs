b2a49f904bfb9c0843cd344c7c4a38aa9dd6750d
Ewgen4ik100 <78424896+Ewgen4ik100@users.noreply.github.com>
2021-07-14 15:27:19 +0300

    Add files via upload

---
+++ Lab8/Lab8/Lab8/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Lab8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите текст: ");
+            string str = Console.ReadLine();
+            string[] arrstr = str.Split(' ');
+            for (int i = 0; i <= arrstr.Length; i++)
+            {
+                if (arrstr[i].Substring(0) != "-" && arrstr[i].Substring(arrstr[i].Length - 1) != "-")
+                {
+                    if (arrstr[i] == arrstr[i + 1])
+                    {
+                        Console.Write(arrstr[i] + " ");
+                    }
+                }
+                else
+                    Console.WriteLine("Слово не может начинаться с дефиса!");
+            }
+        }
+    }
+}
