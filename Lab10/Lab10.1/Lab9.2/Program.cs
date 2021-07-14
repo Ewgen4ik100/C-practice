60256d603bb7d4aaf28759b2bf044dbf239346bd
Ewgen4ik100 <78424896+Ewgen4ik100@users.noreply.github.com>
2021-07-14 15:31:07 +0300

    Add files via upload

---
+++ Lab10/Lab10.1/Lab9.2/Program.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace ConsoleApplicationTest
+{
+    public static class Program
+    {
+        static string path = @"users.txt";
+        static string outs = @"output.txt";
+        
+        private static void Main()
+        {
+            Auth();
+            string[] allLines = File.ReadAllLines(path);
+            Student[] students = new Student[allLines.Length];
+            string[] fields = new string[5];
+            for (int i = 0; i < allLines.Length; i++)
+            {
+                string line = allLines[i];
+                fields = line.Split(';');
+                Student student = new Student(fields[0], fields[1], fields[2], fields[3], fields[4]);
+                students[i] = student;
+            }
+            Array.Sort(students);
+            foreach (Student student in students)
+            {
+                Console.WriteLine(student);
+            }
+            string[] linesToSave = new string[students.Length];
+            for (int i = 0; i < students.Length; i++)
+            {
+                for(int j = 0; j < 5; j++)
+                {
+                    if (students[i].Ocen[j] == 2)
+                    {
+                        Student student = students[i];
+                        linesToSave[i] = student.ToString();
+                    }
+                }
+            }
+            // Сохраняем в файл
+            FileInfo fi = new FileInfo(outs);
+            if (fi.Exists)
+            {
+                File.WriteAllLines(outs, linesToSave, Encoding.UTF8);
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine($"Отсортированные данные записаны в файл {outs}");
+                Console.ResetColor();
+            }
+            else
+            {
+                File.Create(outs);
+                File.WriteAllLines(outs, linesToSave);
+                Console.ReadKey();
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine($"Отсортированные данные записаны в файл {outs}");
+                Console.ResetColor();
+
+            }
+        }
+        public static void Auth()
+        {
+            FileInfo fn = new FileInfo(path);
+            if (fn.Exists)
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine($"Файл {path} существует! Продолжаем работу!");
+                Console.ResetColor();
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine($"Файл {path} не найден! Проверьте расположение файла!");
+                Console.ResetColor();
+            }
+        }
+    }
+    public struct Student : IComparable
+    {
+        public Student(string name, string fac, string group, string cours, string nums)
+            : this()
+        {
+            Name = name;
+            Fac = fac;
+            Group = group;
+            Cours = cours;
+            Nums = nums;
+            string[] str = nums.Split(' ');
+            Ocen = new int[5];
+            for (int i = 0; i < 5; i++)
+            {
+                Ocen[i] = Convert.ToInt32(str[i]);
+            }
+        }
+        public string Name { get; set; }
+        public string Fac { get; set; }
+        public string Group { get; set; }
+        public string Cours { get; set; }
+        public string Nums { get; set; }
+        public int[] Ocen { get; set; }
+        public int CompareTo(object obj)
+        {
+            return Fac.CompareTo(((Student)obj).Fac);
+        }
+        public override string ToString()
+        {
+            return string.Format($"Фамилия: {Name}; Факультет: {Fac}; Группа: {Group}; Курс: {Cours}; Отметки: {Nums}");
+        }
+    }
+}
---
+++ Lab10/Lab10.2/Lab10.2/Program.cs
@@ -0,0 +1,96 @@
+﻿using System;
+
+namespace Lab10._2
+{
+    interface ISequence
+    {
+        void GetElement(int nam);
+        void Sum();
+    }
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Console.Write("Введите элемент: ");
+            int s = int.Parse(Console.ReadLine());
+            GeometricProgression gm = new GeometricProgression();
+            gm.Geometric(2, 2, 10);
+            gm.GetElement(s);
+            gm.Sum();
+            ArithmeticProgression am = new ArithmeticProgression();
+            am.Arithmetic(2, 3, 30);
+            am.Sum();
+            am.GetElement(s);
+        }
+    }
+    public class GeometricProgression : ISequence
+    {
+        int i;
+        public int sum;
+        public int[] arr_a;
+        public void Geometric(int uan, int tm, int n)
+        {
+            Console.Write("Ваши элементы геометрической прогресси: ");
+            arr_a = new int[n];
+            for (i = 0; i < n; i++,uan*=tm)
+            {
+                arr_a[i] = uan * tm;
+                Console.Write(arr_a[i] + " ");
+            }
+            Console.WriteLine();
+        }
+        public void Sum()
+        {
+            Console.Write("До какого элемента вывести сумму? ");
+            int kon = int.Parse(Console.ReadLine());
+            int sum = 0;
+            for(i = 0; i < kon; i++)
+            {
+                sum += arr_a[i];
+            }
+            Console.WriteLine($"GF: Сумма элементов до элемента под №{kon} равна: {sum}");
+        }
+        public void GetElement(int nam)
+        {
+            int nams = arr_a[nam];
+            Console.WriteLine($"GF: Под номером {nam}, лежит {nams} элемент");
+        }
+    }
+    public class ArithmeticProgression : ISequence
+    {
+         
+        public int[] arr_a;
+        public void Arithmetic(int uan, int razn, int n)
+        {
+            Console.Write("Ваши элементы арифметической прогресси: ");
+            arr_a = new int[n];
+            for (int i = 0; i < n; i++, uan += razn)
+            {
+                arr_a[i] = uan + razn;
+                Console.Write(arr_a[i] + " ");
+            }
+            Console.WriteLine();
+        }
+        public void Sum()
+        {
+            Console.Write("До какого элемента вывести сумму? ");
+            int kon = int.Parse(Console.ReadLine());
+            int sum = 0;
+            for (int i = 0; i < kon; i++)
+            {
+                sum += arr_a[i];
+            }
+            Console.WriteLine($"AF: Сумма элементов до элемента под №{kon} равна: {sum}");
+        }
+        public void GetElement(int nam)
+        {
+            int nams = arr_a[nam];
+            Console.WriteLine($"AF: Под номером {nam}, лежит {nams} элемент");
+        }
+    }
+}
+
+
+
+
