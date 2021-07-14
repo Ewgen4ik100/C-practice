35d967eeb677ef79d9af68a35ec454149afd2493
Ewgen4ik100 <78424896+Ewgen4ik100@users.noreply.github.com>
2021-07-14 15:43:25 +0300

    Add files via upload

---
+++ Lab19/Lab19.1/Lab19.1/Program.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Lab19._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите кол-во сторон многоугольника: ");
+            int n = int.Parse(Console.ReadLine());
+            Totchka(n);
+        }
+        public static void Totchka(int n)
+        {
+            List<Point> point = new List<Point>();
+            for (int i = 0; i < n; i++)
+            {
+                point.Add(new Point());
+                Console.Write($"Введите координату Х{i + 1}: ");
+                point[i].X = int.Parse(Console.ReadLine());
+                Console.Write($"Введите координату Y{i + 1} : ");
+                point[i].Y = int.Parse(Console.ReadLine());
+            }
+            for (int i = 0; i < n; i++)
+            {
+                point[i].GetInfo();
+            }
+        }
+    }
+    public class Point
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+        public Point() { }
+        public Point(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+        public virtual void GetInfo()
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine($"Координаты вершин: {X}, {Y}");
+            Console.ResetColor();
+        }
+    }
+    public class ColoredPoint:Point
+    {
+        public string ColorP { get; set; }
+        public ColoredPoint(int x, int y, string colorp):base(x,y)
+        {
+            ColorP = colorp;
+        }
+        public override void GetInfo()
+        {
+            Console.WriteLine($"Координаты точки: {X}, {Y}; Цвет: {ColorP}");
+        }
+    }
+    public class Line:Point
+    {
+        public int Xl { get; set; }
+        public int Yl { get; set; }
+        public Line(int xl, int yl, int x, int y) : base(x, y)
+        {
+            Xl = xl;
+            Yl = yl;
+        }
+        public override void GetInfo()
+        {
+            Console.WriteLine($"Координаты точки №1: {X}, {Y}; Координаты точки №2: {Xl}, {Yl}");
+        }
+    }
+    public class ColoredLine:Line
+    {
+        public string ColorL { get; set; }
+        public ColoredLine(int xl, int yl, int x, int y, string colorl):base(xl, yl, x, y)
+        {
+            ColorL = colorl;
+        }
+        public override void GetInfo()
+        {
+            Console.WriteLine($"Координаты точки №1: {X}, {Y}; Координаты точки №2: {Xl}, {Yl}; Цвет: {ColorL}");
+        }
+    }
+    public class PolyLine:Line
+    {
+        public List<string> PolL;
+        public PolyLine(List<string> poll, int x, int y, int xl, int yl) : base(x, y, xl, yl)
+        {
+            PolL = poll;
+        }
+        public override void GetInfo()
+        {
+            Console.WriteLine($"Координаты точки №1: {X}, {Y}; Координаты точки №2: {Xl}, {Yl};");
+        }
+    }
+}
---
+++ Lab19/Lab19.2/Lab19.2/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Lab19._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите номер поезда: ");
+            string nam = Console.ReadLine();
+            Trains(nam);
+        }
+        public static void Trains(string nam)
+        {
+            List<Train> tr = new List<Train>();
+            for (int i = 0; i < 8; i++)
+            {
+                Console.Write($"Введите номер поезда №{i + 1}: ");
+                string NumP = Console.ReadLine();
+                Console.Write("Введите пункт назначения: ");
+                string PunktN = Console.ReadLine();
+                Console.Write("Введите время отправления: ");
+                string Time = Console.ReadLine();
+                tr.Add(new Train(PunktN, NumP, Time));
+            }
+            var sortTrain = from t in tr orderby t.NumP select t;
+            Console.WriteLine("Отсортированные поезда: ");
+            foreach(Train t in tr)
+            {
+                t.GetInfo();
+            }
+            Console.WriteLine("Найденные поезда: ");
+            bool val = false;
+            foreach (Train t in sortTrain)
+            {
+                if (t.NumP == nam)
+                {
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    t.GetInfo();
+                    Console.ResetColor();
+                    val = true;
+                }
+            }
+            if(val==false)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine($"Поездов с номером {nam} не существует!");
+                Console.ResetColor();
+            }
+        }
+    }
+    public struct Train
+    {
+        public string PunktN { get; set; }
+        public string NumP { get; set; }
+        public string Time { get; set; }
+        public Train(string punktn, string nump, string time)
+        {
+            PunktN = punktn;
+            NumP = nump;
+            Time = time;
+        }
+        public void GetInfo()
+        {
+            Console.WriteLine($"Пункт назначения: {PunktN}; Номер поезда: {NumP}; Время отправления: {Time}");
+        }
+    }
+    
+}
