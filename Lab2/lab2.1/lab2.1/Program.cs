b38317375f70f5bc86a03558befb833cb6c64e51
Ewgen4ik100 <78424896+Ewgen4ik100@users.noreply.github.com>
2021-07-14 15:22:22 +0300

    Add files via upload
    Lab2

---
+++ Lab2/lab2.1/lab2.1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace lab2._1
+{
+    class Program
+    {
+        static void Main(string[] args) 
+        { 
+            double a = -1.5;
+            double b = 15.5;
+            double x = -2.9;
+            double w = Math.Sqrt(Math.Pow(x, 2) + b) - Math.Pow(b, 2) * Math.Pow(Math.Sin((x + a) / x), 3);
+            Console.WriteLine("W = " + string.Format("{0:N2}", w));
+            double y = Math.Pow(Math.Cos(Math.Pow(x, 3)), 2) - (x / (Math.Sqrt(Math.Pow(a, 2) + Math.Pow(b, 2))));
+            Console.WriteLine("Y = " + string.Format("{0:N2}", y));
+        }
+    }
+}
---
+++ Lab2/lab2.2/lab2.2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+namespace lab2._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите N: ");
+            double n = double.Parse(Console.ReadLine());
+            Console.Write("Введите X: ");
+            double x = double.Parse(Console.ReadLine());
+            Console.Write("Введите B: ");
+            double b = double.Parse(Console.ReadLine());
+            Console.Write("Введите A: ");
+            double a = double.Parse(Console.ReadLine());
+            double p = Primer(n, x, a, b);
+            Console.WriteLine(p);
+        }
+        static double Primer(double n, double x, double a, double b)
+        {
+            if (n >= 2)
+            {
+                return ((Math.Exp(a * x) * Math.Pow(Math.Cos(x), 2) * (a * Math.Cos(x) + n * Math.Sin(x)) / (Math.Pow(a, 2) + Math.Pow(n, 2)))) * Primer(n - 2, x, a, b);
+            }
+            else if (n == 1)
+            {
+                return ((-Math.Exp(a * x) * (Math.Sin(x) + Math.Cos(x))) / (Math.Pow(a,2) + Math.Pow(n, 2)));
+            }
+            else if (n == 0)
+            {
+                return ((Math.Exp(a * x) / a));
+            }
+            return 0;
+        }
+    }
+}
