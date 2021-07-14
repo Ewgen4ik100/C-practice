18d9eead04c75beaac780a26351e3840103b3845
Ewgen4ik100 <78424896+Ewgen4ik100@users.noreply.github.com>
2021-07-14 15:34:39 +0300

    Add files via upload

---
+++ Lab13/Lab13.1/Lab13.1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Lab13._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char x1 = Convert.ToChar("Д");
+            char x2 = Convert.ToChar("р");
+            Console.WriteLine("Hello World!");
+            Action < Func<int>, char, char> op = Test2;
+            Func<int> fc = Test1;
+            fc.Invoke();
+            op.Invoke(fc, x1, x2);
+        }
+        static public void Test2(Func<int> t, char ch, char ch2)
+        {
+            Console.WriteLine($"{ch}елегат - {ch2}еализован!");
+        }
+        static public int Test1()
+        {
+            return 2;
+        }
+    }
+}
---
+++ Lab13/Lab13.2/Lab13.2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Text;
+
+namespace Lab13._2
+{
+    delegate void Meth();
+    class Program
+    {
+        const string alfabet = "АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ";
+        static void Main(string[] args)
+        {
+            Console.Write("Введите строку для шифровки: ");
+            string stroka = Console.ReadLine();
+            Console.Write("Введите ключ для сдвига: ");
+            int n = int.Parse(Console.ReadLine());
+            Meth mt = () => Method(stroka, n);
+            mt();
+        }
+        public static void Method(string str, int n)
+        {
+            var rez = new StringBuilder();
+            string alf = alfabet + alfabet.ToLower();
+            for(int i = 0; i < str.Length; i++)
+            {
+                for(int j = 0; j < alf.Length; j++)
+                {
+                    if(str[i] == alf[j])
+                    {
+                        rez.Append(alf[j + n]);
+                    }
+                }
+            }
+            Console.WriteLine($"Зашифрованый текст: {rez.ToString()}");
+        }
+    }
+}
